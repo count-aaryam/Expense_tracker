30aa86a7600ac2b8542bfe2bee65365174006760
count-aaryam <aaryamsingh2005@gmail.com>
2026-06-21 16:49:30 +0000

    new product

--- expense-tracker.py
+++ expense-tracker.py
@@ -1,11 +1,40 @@
-from tabulate import tabulate 
-import json
+import sys
+import utils
 
-with open("expenses.json","r") as file:
-    data=json.load(file)
+commands={
+    "add":4,
+    "list":2,
+    "delete":3,
+    "summary":2
+}
 
-print(tabulate(data,tablefmt="grid",headers="keys"))
+list_operations=("add","list","summary","delete")
 
-for row in data:
-    amount=int(row["Amount"][:len(row["Amount"])-1])
-    print(amount)
+args=sys.argv
+
+if len(args)==1:
+    utils.print_task_list()
+else:
+    cmd=args[1]
+
+    if cmd not in commands:
+        print("Unknown Command")
+        exit()
+
+    required_len=commands[cmd]
+
+    if len(args)<required_len:
+        print("Not enough arguements for the operation")
+        exit()
+
+    if cmd=="add":
+        desc=args[2]
+        cost=args[3]
+        utils.add_expense(desc,cost)
+    if cmd=="delete":
+        id=int(args[2])
+        utils.delete_spend_rec(id)
+    if cmd=="summary":
+        utils.report_summary()
+    if cmd=="list":
+        utils.print_list()
